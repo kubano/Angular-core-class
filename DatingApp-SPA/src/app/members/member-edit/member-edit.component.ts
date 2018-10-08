403aa6e1b1254ef96a111106d988007b672c14dd
Cesar L. Diaz (kubano) <kubano@outlook.com>
2018-10-08 17:03:27 -0400

    edit profile ongoing work

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../_models/user';
+import { AlertifyService } from '../../_services/alertify.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  user: User;
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+updateUser() {
+  console.log(this.user);
+  this.alertify.success('Updated User!');
+}
+}
